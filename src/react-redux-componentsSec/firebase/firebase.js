79b8ba02ebf1df3a810fa38f4199d5417a42e9a9
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2019-10-25 00:00:47 +0900

    practice

--- src/react-redux-componentsSec/firebase/firebase.js
+++ src/react-redux-componentsSec/firebase/firebase.js
@@ -1,4 +1,5 @@
-import * as firebase from "firebase";
+import * as firebase from "firebase/app";
+import "firebase/database";
 
 const config = {};
 
@@ -10,6 +11,39 @@ firebase
   .set({
     name: "shu",
     age: 28
+  })
+  .then(() => {
+    console.log("ok");
+  })
+  .catch(e => {
+    console.log("error", e);
+  });
+
+firebase
+  .database()
+  .ref("name")
+  .remove()
+  .then(function() {
+    console.log("Remove succeeded.");
+  })
+  .catch(function(error) {
+    console.log("Remove failed: " + error.message);
+  });
+
+firebase
+  .database()
+  .ref()
+  .update({name: "hello"});
+
+firebase
+  .database()
+  .ref("name")
+  .once("value")
+  .then(data => {
+    console.log(data.val());
+  })
+  .catch(e => {
+    console.log("error");
   });
 
 // firebase
